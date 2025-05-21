0c735c7691ea6e7acbd27a14259a89f5a3d729f7
Gabriel Cramer <cramergabriel@gmail.com>
2025-05-21 05:09:04 +0300

    Update colors and remove clean function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,5 @@
 'use strict';
 
-function clean(node) {
-  for (var n = 0; n < node.childNodes.length; n++) {
-    var child = node.childNodes[n];
-    if (child.nodeType === 8 || child.nodeType === 3 && !/\S/.test(child.nodeValue)) {
-      node.removeChild(child);
-      n--;
-    } else if (child.nodeType === 1) {
-      clean(child);
-    }
-  }
-}
-clean(document);
-
 function addClass(element, className) {
   if (element.classList) {
     element.classList.add(className);
